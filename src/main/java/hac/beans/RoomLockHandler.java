7d64c0036f21626d709d3ef49fbababa3be56398
EliadKarni <eliadk995@gmail.com>
2023-06-30 18:42:20 +0300

    Room lock handler doc

--- src/main/java/hac/beans/RoomLockHandler.java
+++ src/main/java/hac/beans/RoomLockHandler.java
@@ -9,22 +9,44 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * The component manages a dict of locks so each room in the database will have its own lock.
+ */
 @Component
 public class RoomLockHandler implements Serializable {
+    /**
+     * The rooms' lock dict.
+     */
     @Resource(name = "getRoomLocksMap")
     private HashMap<Long, ReentrantReadWriteLock> roomsLock;
 
+    /**
+     * Ctor.
+     */
     public RoomLockHandler() {
     }
 
+    /**
+     * The rooms' locks dict getter.
+     * @return The rooms' locks dict.
+     */
     public HashMap<Long, ReentrantReadWriteLock> getRoomsLock() {
         return roomsLock;
     }
 
+    /**
+     * The function is a setter of the rooms' locks dict.
+     * @param roomsLock The new rooms' locks dict.
+     */
     public void setRoomsLock(HashMap<Long, ReentrantReadWriteLock> roomsLock) {
         this.roomsLock = roomsLock;
     }
 
+    /**
+     * The function returns the room's lock which the received room id belongs to.
+     * @param id The id of the room lock belongs to.
+     * @return The room's lock which the id belongs to.
+     */
     public ReentrantReadWriteLock getRoomLock(Long id){
         ReentrantReadWriteLock readWriteLock = roomsLock.get(id);
         if (readWriteLock == null)
@@ -32,6 +54,10 @@ public class RoomLockHandler implements Serializable {
         return roomsLock.get(id);
     }
 
+    /**
+     * The function adds to the dict a new lock which its key in the dict is the room's id.
+     * @param id The room's id.
+     */
     public void setNewRoomLock(Long id){
         ReentrantReadWriteLock readWriteLock = roomsLock.get(id);
         if (readWriteLock != null)
@@ -39,6 +65,10 @@ public class RoomLockHandler implements Serializable {
         roomsLock.put(id, new ReentrantReadWriteLock());
     }
 
+    /**
+     * The function removes the lock which it's key in the dict is the received parameter.
+     * @param id The id of the room its lock need to get removed.
+     */
     public void removeLock(Long id){
         roomsLock.remove(id);
     }
