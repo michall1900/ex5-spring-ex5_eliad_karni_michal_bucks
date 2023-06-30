cea9781389718f4518ba8b0254de09afa34ad550
EliadKarni <eliadk995@gmail.com>
2023-06-30 17:33:35 +0300

    Users client side validation fix

--- src/main/java/hac/controllers/LoginController.java
+++ src/main/java/hac/controllers/LoginController.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 
 @Controller
 public class LoginController {
@@ -33,16 +31,6 @@ public class LoginController {
         model.addAttribute("mode", "login");
         return "loginPages/login-register";
     }
-
-    /** User zone index. */
-    @RequestMapping("/user")
-    public String userIndex() {
-        return "user/index";
-    }
-
-    /** User zone index. */
-
-
     @RequestMapping("/register/{errorMessage}")
     public String getRegisterErrorMessage(@RequestParam("errorMessage") String errorMessage, Model model) {
         model.addAttribute("mode", "register");
@@ -51,7 +39,7 @@ public class LoginController {
     }
 
     @RequestMapping("/register")
-    public String getRegister(String errorMessage, Model model) {
+    public String getRegister(Model model) {
         model.addAttribute("mode", "register");
         return "loginPages/login-register";
     }
@@ -72,41 +60,6 @@ public class LoginController {
             model.addAttribute("errorMessage", e.getMessage());
             return "loginPages/login-register";
         }
-    }
-    /** Administration zone index.
-     * Note that we can access current logged user just by adding the Principal
-     * parameter
-     */
-    @RequestMapping("/admin")
-    public String adminIndex(Principal principal) {
-        //System.out.println("Current logged user details: " + " (" + principal + ")" );
-        return "admin/index";
-    }
-
-    /** Shared zone index. */
-    @RequestMapping("/shared")
-    public String sharedIndex() {
-        // print current logged user details - withouth using the Principal parameter
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        System.out.println("Current logged user details: "
-//                + ((UserDetails) principal).getUsername()
-//                + " (" + ((UserDetails) principal).getAuthorities() + ")");
-
-
-        return "shared/index";
-    }
-
-    /** Simulation of an exception. */
-    @RequestMapping("/simulateError")
-    public void simulateError() {
-        throw new RuntimeException("This is a simulated exception thrown by a controller");
-    }
-
-    /** Error page that displays all exceptions. */
-    @RequestMapping("/errorpage")
-    public String error(Exception ex, Model model) {
-        model.addAttribute("errorMessage", ex.getMessage());
-        return "error";
     }
 
     /** simple Error page. */
